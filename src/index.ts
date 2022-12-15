26ce628a0bb9d0123d8263958070a3b19fecf718
Volha Haiduk <Alisatonks2385@gmail.com>
2022-12-15 10:24:23 +0100

    feat: add header

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
 import "./sass/style.scss";
+import { TopHeader, BottomHeader } from "./components/header";
+
+const headerTop = new TopHeader();
+headerTop.drawElements();
+const headerBottom = new BottomHeader();
+headerBottom.drawElements();
