ff69b9aa95f88705acbef3e1962b03be78683872
oolenkazolot <oolenka.zolot@gmail.com>
2022-12-17 15:32:48 +0100

    fix: conflict

--- src/components/filter-range.ts
+++ src/components/filter-range.ts
@@ -7,20 +7,38 @@ class FilterRange {
   private displayValueTwo: HTMLElement;
   constructor(prefix: string) {
     this.prefix = prefix;
-    this.sliderOne = this.createSliderInput('0', '100', `${this.prefix}-slider-1`, '30', this.slideOne.bind(this));
-    this.sliderTwo = this.createSliderInput('0', '100', `${this.prefix}-slider-2`, '70', this.slideTwo.bind(this));
-    this.displayValueOne = this.createFilterDisplayValue(`${this.prefix}-range1`, '0');
-    this.displayValueTwo = this.createFilterDisplayValue(`${this.prefix}-range2`, '100');
+    this.sliderOne = this.createSliderInput(
+      "0",
+      "100",
+      `${this.prefix}-slider-1`,
+      "30",
+      this.slideOne.bind(this)
+    );
+    this.sliderTwo = this.createSliderInput(
+      "0",
+      "100",
+      `${this.prefix}-slider-2`,
+      "70",
+      this.slideTwo.bind(this)
+    );
+    this.displayValueOne = this.createFilterDisplayValue(
+      `${this.prefix}-range1`,
+      "0"
+    );
+    this.displayValueTwo = this.createFilterDisplayValue(
+      `${this.prefix}-range2`,
+      "100"
+    );
     this.sliderTrack = this.createSliderTrack();
   }
 
   public createFilterRange(): HTMLElement {
-    const rangeFilter: HTMLElement = document.createElement('div');
-    rangeFilter.classList.add('filter-range');
+    const rangeFilter: HTMLElement = document.createElement("div");
+    rangeFilter.classList.add("filter-range");
     rangeFilter.append(this.sliderTrack, this.sliderOne, this.sliderTwo);
     const slideOne = this.slideOne.bind(this);
     const slideTwo = this.slideTwo.bind(this);
-    window.addEventListener('load', () => {
+    window.addEventListener("load", () => {
       slideOne();
       slideTwo();
     });
@@ -28,43 +46,52 @@ class FilterRange {
   }
 
   private createSliderTrack(): HTMLElement {
-    const filterSliderTrack: HTMLElement = document.createElement('div');
-    filterSliderTrack.classList.add('filter-range__slider-track');
+    const filterSliderTrack: HTMLElement = document.createElement("div");
+    filterSliderTrack.classList.add("filter-range__slider-track");
     return filterSliderTrack;
   }
 
-  private createSliderInput(min: string, max: string, id: string, value: string, cb: () => void): HTMLInputElement {
-    const input: HTMLInputElement = document.createElement('input');
-    input.classList.add('filter-range__input');
-    input.setAttribute('type', 'range');
-    input.setAttribute('min', min);
-    input.setAttribute('max', max);
-    input.setAttribute('value', value);
-    input.setAttribute('id', id);
-    input.addEventListener('input', cb);
+  private createSliderInput(
+    min: string,
+    max: string,
+    id: string,
+    value: string,
+    cb: () => void
+  ): HTMLInputElement {
+    const input: HTMLInputElement = document.createElement("input");
+    input.classList.add("filter-range__input");
+    input.setAttribute("type", "range");
+    input.setAttribute("min", min);
+    input.setAttribute("max", max);
+    input.setAttribute("value", value);
+    input.setAttribute("id", id);
+    input.addEventListener("input", cb);
     return input;
   }
 
   public createFilterValues(): HTMLElement {
-    const filterValues: HTMLElement = document.createElement('div');
-    filterValues.classList.add('filter-values');
-    const span2: HTMLElement = document.createElement('span');
-    span2.textContent = '-';
+    const filterValues: HTMLElement = document.createElement("div");
+    filterValues.classList.add("filter-values");
+    const span2: HTMLElement = document.createElement("span");
+    span2.textContent = "-";
     filterValues.append(this.displayValueOne, span2, this.displayValueTwo);
     return filterValues;
   }
 
   private createFilterDisplayValue(id: string, value: string): HTMLElement {
-    const span: HTMLElement = document.createElement('span');
-    span.setAttribute('id', id);
+    const span: HTMLElement = document.createElement("span");
+    span.setAttribute("id", id);
     span.textContent = value;
     return span;
   }
 
   private slideOne(): void {
     const minGap = 0;
-    if (parseInt(this.sliderTwo.value) - parseInt(this.sliderOne.value) <= minGap) {
-      this.sliderOne.value = parseInt(this.sliderTwo.value) - minGap + '';
+    if (
+      parseInt(this.sliderTwo.value) - parseInt(this.sliderOne.value) <=
+      minGap
+    ) {
+      this.sliderOne.value = parseInt(this.sliderTwo.value) - minGap + "";
     }
     this.displayValueOne.textContent = this.sliderOne.value;
     this.fillColor();
@@ -72,8 +99,11 @@ class FilterRange {
 
   private slideTwo(): void {
     const minGap = 0;
-    if (parseInt(this.sliderTwo.value) - parseInt(this.sliderOne.value) <= minGap) {
-      this.sliderTwo.value = parseInt(this.sliderOne.value) + minGap + '';
+    if (
+      parseInt(this.sliderTwo.value) - parseInt(this.sliderOne.value) <=
+      minGap
+    ) {
+      this.sliderTwo.value = parseInt(this.sliderOne.value) + minGap + "";
     }
     this.displayValueTwo.textContent = this.sliderTwo.value;
     this.fillColor();
--- src/components/filter.ts
+++ src/components/filter.ts
@@ -1,6 +1,6 @@
-import { IProducts, IFilterRange } from '../types/index';
-import Products from '../utils/products';
-import FilterRange from './filter-range';
+import { IProducts, IFilterRange } from "../types/index";
+import Products from "../utils/products";
+import FilterRange from "./filter-range";
 
 class Filter {
   private products: IProducts;
@@ -8,24 +8,24 @@ class Filter {
   private filterRange2: IFilterRange;
   constructor() {
     this.products = new Products();
-    this.filterRange1 = new FilterRange('price');
-    this.filterRange2 = new FilterRange('stock');
+    this.filterRange1 = new FilterRange("price");
+    this.filterRange2 = new FilterRange("stock");
   }
 
   public drawFilter(): HTMLElement {
-    const filterElement: HTMLElement = document.createElement('aside');
-    filterElement.classList.add('filter');
-    const filterTitle: HTMLElement = document.createElement('h2');
-    filterTitle.classList.add('filter__title');
-    filterTitle.textContent = 'Filter';
+    const filterElement: HTMLElement = document.createElement("aside");
+    filterElement.classList.add("filter");
+    const filterTitle: HTMLElement = document.createElement("h2");
+    filterTitle.classList.add("filter__title");
+    filterTitle.textContent = "Filter";
     const filterWrap = this.createFilterWrap();
     filterElement.append(filterTitle, filterWrap);
     return filterElement;
   }
 
   private createFilterWrap(): HTMLElement {
-    const filterWrap: HTMLElement = document.createElement('div');
-    filterWrap.classList.add('filter__wrap');
+    const filterWrap: HTMLElement = document.createElement("div");
+    filterWrap.classList.add("filter__wrap");
     const blockCategories: HTMLElement = this.createFilterBlockCategories();
     const blockBrands: HTMLElement = this.createFilterBlockBrands();
     const blockPrice: HTMLElement = this.createFilterBlockPrice();
@@ -35,11 +35,11 @@ class Filter {
   }
 
   private createFilterBlockCategories(): HTMLElement {
-    const block: HTMLElement = document.createElement('div');
-    block.classList.add('filter-block');
-    const title: HTMLElement = document.createElement('h3');
-    title.classList.add('filter-block__title');
-    title.textContent = 'Category';
+    const block: HTMLElement = document.createElement("div");
+    block.classList.add("filter-block");
+    const title: HTMLElement = document.createElement("h3");
+    title.classList.add("filter-block__title");
+    title.textContent = "Category";
     const obj: Record<string, number> = this.products.getCategoriesObject();
     const blockItems = this.createFilterBlockItems(obj);
     block.append(title, blockItems);
@@ -47,26 +47,26 @@ class Filter {
   }
 
   private createFilterBlockItems(obj: Record<string, number>): HTMLElement {
-    const blockItems: HTMLElement = document.createElement('div');
-    blockItems.classList.add('filter-block__items');
+    const blockItems: HTMLElement = document.createElement("div");
+    blockItems.classList.add("filter-block__items");
     for (const key in obj) {
-      const item: HTMLElement = document.createElement('div');
-      item.classList.add('filter-block__item');
-      const label: HTMLElement = document.createElement('label');
-      label.classList.add('filter-block__checkbox');
-      const input: HTMLElement = document.createElement('input');
-      input.classList.add('filter-block__input');
-      input.setAttribute('id', key);
-      input.setAttribute('type', 'checkbox');
-      const customCheckbox: HTMLElement = document.createElement('span');
-      customCheckbox.classList.add('filter-block__custom-checkbox');
-      const span: HTMLElement = document.createElement('span');
-      span.classList.add('filter-block__text');
+      const item: HTMLElement = document.createElement("div");
+      item.classList.add("filter-block__item");
+      const label: HTMLElement = document.createElement("label");
+      label.classList.add("filter-block__checkbox");
+      const input: HTMLElement = document.createElement("input");
+      input.classList.add("filter-block__input");
+      input.setAttribute("id", key);
+      input.setAttribute("type", "checkbox");
+      const customCheckbox: HTMLElement = document.createElement("span");
+      customCheckbox.classList.add("filter-block__custom-checkbox");
+      const span: HTMLElement = document.createElement("span");
+      span.classList.add("filter-block__text");
       span.textContent = key;
       label.append(input, customCheckbox, span);
-      const count: HTMLElement = document.createElement('span');
-      count.classList.add('filter-block__count');
-      count.textContent = '(' + obj[key] + '/' + obj[key] + ')';
+      const count: HTMLElement = document.createElement("span");
+      count.classList.add("filter-block__count");
+      count.textContent = "(" + obj[key] + "/" + obj[key] + ")";
       item.append(label, count);
       blockItems.append(item);
     }
@@ -74,11 +74,11 @@ class Filter {
   }
 
   private createFilterBlockBrands(): HTMLElement {
-    const block: HTMLElement = document.createElement('div');
-    block.classList.add('filter-block');
-    const title: HTMLElement = document.createElement('h3');
-    title.classList.add('filter-block__title');
-    title.textContent = 'Brand';
+    const block: HTMLElement = document.createElement("div");
+    block.classList.add("filter-block");
+    const title: HTMLElement = document.createElement("h3");
+    title.classList.add("filter-block__title");
+    title.textContent = "Brand";
     const obj: Record<string, number> = this.products.getBrandsObject();
     const blockItems = this.createFilterBlockItems(obj);
     block.append(title, blockItems);
@@ -86,11 +86,11 @@ class Filter {
   }
 
   private createFilterBlockPrice(): HTMLElement {
-    const block: HTMLElement = document.createElement('div');
-    block.classList.add('filter-block');
-    const title: HTMLElement = document.createElement('h3');
-    title.classList.add('filter-block__title');
-    title.textContent = 'Price';
+    const block: HTMLElement = document.createElement("div");
+    block.classList.add("filter-block");
+    const title: HTMLElement = document.createElement("h3");
+    title.classList.add("filter-block__title");
+    title.textContent = "Price";
     const rangeFilter: HTMLElement = this.filterRange1.createFilterRange();
     const filterValues: HTMLElement = this.filterRange1.createFilterValues();
     block.append(title, filterValues, rangeFilter);
@@ -98,11 +98,11 @@ class Filter {
   }
 
   private createFilterBlockStock(): HTMLElement {
-    const block: HTMLElement = document.createElement('div');
-    block.classList.add('filter-block');
-    const title: HTMLElement = document.createElement('h3');
-    title.classList.add('filter-block__title');
-    title.textContent = 'Stock';
+    const block: HTMLElement = document.createElement("div");
+    block.classList.add("filter-block");
+    const title: HTMLElement = document.createElement("h3");
+    title.classList.add("filter-block__title");
+    title.textContent = "Stock";
     const rangeFilter: HTMLElement = this.filterRange2.createFilterRange();
     const filterValues: HTMLElement = this.filterRange2.createFilterValues();
     block.append(title, filterValues, rangeFilter);
