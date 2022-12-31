3f23562025b21ca5360df8a7b5630d88a79a8b1f
oolenkazolot <oolenka.zolot@gmail.com>
2022-12-31 14:37:01 +0100

    feat: implement text search

--- src/components/products-list.ts
+++ src/components/products-list.ts
@@ -1,5 +1,5 @@
-import { IProducts, IProduct, IRouter } from "../types/index";
-import Products from "../utils/products";
+import { IProducts, IProduct, IRouter } from '../types/index';
+import Products from '../utils/products';
 
 class ProductsList {
   private products: IProducts;
@@ -7,50 +7,53 @@ class ProductsList {
 
   constructor() {
     this.products = new Products();
-    this.productsList = document.createElement("div");
+    this.productsList = document.createElement('div');
   }
 
   public createProductsList(router?: IRouter): HTMLElement {
-    this.productsList.classList.add("products__list");
+    this.productsList.classList.add('products__list');
     const productsElements = this.createProductsElement(router);
     this.productsList.append(...productsElements);
     return this.productsList;
   }
 
   public draw(router?: IRouter): void {
-    this.productsList.textContent = "";
+    this.productsList.textContent = '';
     const productsElements = this.createProductsElement(router);
     this.productsList.append(...productsElements);
   }
 
   private createProductsElement(router?: IRouter): HTMLElement[] {
-    const products: IProduct[] = this.products.getProductsFilters();
+    const url = new URL(window.location.href);
+    const urlParameterSearch: string | null = url.searchParams.get('search');
+    let products: IProduct[] = this.products.getProductsFilters();
+
+    if (products.length && !urlParameterSearch) {
+      products = this.products.getProductsFilters();
+    }
+
+    if (products.length && urlParameterSearch) {
+      products = this.products.getProductsFiltersSearch();
+    }
+
     if (!products.length) {
-      const productElement: HTMLElement = document.createElement("div");
-      productElement.classList.add("products__not-found");
-      productElement.textContent = "No products found 😏";
-      this.productsList.classList.add("products__list--block");
+      const productElement: HTMLElement = document.createElement('div');
+      productElement.classList.add('products__not-found');
+      productElement.textContent = 'No products found 😏';
+      this.productsList.classList.add('products__list--block');
       return [productElement];
     }
-    this.productsList.classList.remove("products__list--block");
+    this.productsList.classList.remove('products__list--block');
     const productsElements = products.map(
       (item: IProduct, index: number): HTMLElement => {
-        const productElement: HTMLElement = document.createElement("div");
-        productElement.classList.add("product");
-        productElement.setAttribute("data-id", index + "");
+        const productElement: HTMLElement = document.createElement('div');
+        productElement.classList.add('product');
+        productElement.setAttribute('data-id', index + '');
         const productName: HTMLElement = this.createProductName(item);
         const productImg: HTMLElement = this.createProductImg(item);
         const productInfo: HTMLElement = this.createProductInfo(item);
-        const productBtnWrap: HTMLElement = this.createProductBtnWrap(
-          item,
-          router
-        );
-        productElement.append(
-          productName,
-          productImg,
-          productInfo,
-          productBtnWrap
-        );
+        const productBtnWrap: HTMLElement = this.createProductBtnWrap(item, router);
+        productElement.append(productName, productImg, productInfo, productBtnWrap);
         return productElement;
       }
     );
@@ -58,37 +61,37 @@ class ProductsList {
   }
 
   private createProductName(item: IProduct): HTMLElement {
-    const productName: HTMLElement = document.createElement("div");
-    productName.classList.add("product__name");
-    const productBrand: HTMLElement = document.createElement("span");
-    productBrand.classList.add("product__brand");
+    const productName: HTMLElement = document.createElement('div');
+    productName.classList.add('product__name');
+    const productBrand: HTMLElement = document.createElement('span');
+    productBrand.classList.add('product__brand');
     productBrand.textContent = item.brand;
-    const productModel: HTMLElement = document.createElement("span");
-    productModel.classList.add("product__model");
+    const productModel: HTMLElement = document.createElement('span');
+    productModel.classList.add('product__model');
     productModel.textContent = item.title;
     productName.append(productBrand, productModel);
     return productName;
   }
 
   private createProductImg(item: IProduct): HTMLElement {
-    const productImg: HTMLElement = document.createElement("div");
-    productImg.classList.add("product__display");
-    const img: HTMLElement = document.createElement("img");
-    img.classList.add("product__img");
-    img.setAttribute("src", item.thumbnail);
-    img.setAttribute("alt", "product-img");
+    const productImg: HTMLElement = document.createElement('div');
+    productImg.classList.add('product__display');
+    const img: HTMLElement = document.createElement('img');
+    img.classList.add('product__img');
+    img.setAttribute('src', item.thumbnail);
+    img.setAttribute('alt', 'product-img');
     productImg.append(img);
     return productImg;
   }
 
   private createProductInfo(item: IProduct): HTMLElement {
-    const arr = ["category", "discount", "rating", "stock"];
-    const productInfo: HTMLElement = document.createElement("div");
-    productInfo.classList.add("product__info");
+    const arr = ['category', 'discount', 'rating', 'stock'];
+    const productInfo: HTMLElement = document.createElement('div');
+    productInfo.classList.add('product__info');
     const productsInfoItems = arr.map((elem: string) => {
-      const span: HTMLElement = document.createElement("span");
-      span.classList.add("product__" + elem);
-      span.textContent = elem + ": " + item[elem as keyof IProduct];
+      const span: HTMLElement = document.createElement('span');
+      span.classList.add('product__' + elem);
+      span.textContent = elem + ': ' + item[elem as keyof IProduct];
       return span;
     });
     productInfo.append(...productsInfoItems);
@@ -96,11 +99,11 @@ class ProductsList {
   }
 
   private createProductBtnWrap(item: IProduct, router?: IRouter): HTMLElement {
-    const productBtnWrap: HTMLElement = document.createElement("div");
-    productBtnWrap.classList.add("product__btn-wrap");
-    const productPrice: HTMLElement = document.createElement("h3");
-    productPrice.classList.add("product__price");
-    productPrice.textContent = "€" + item.price;
+    const productBtnWrap: HTMLElement = document.createElement('div');
+    productBtnWrap.classList.add('product__btn-wrap');
+    const productPrice: HTMLElement = document.createElement('h3');
+    productPrice.classList.add('product__price');
+    productPrice.textContent = '€' + item.price;
     const btnDetails = this.createBtnDetails(item.id, router);
     const btnCart = this.createBtnCart();
     productBtnWrap.append(productPrice, btnDetails, btnCart);
@@ -108,11 +111,11 @@ class ProductsList {
   }
 
   private createBtnDetails(id: number, router?: IRouter): HTMLElement {
-    const btnDetails: HTMLElement = document.createElement("button");
-    btnDetails.classList.add("product__btn-details");
-    btnDetails.textContent = "Details";
+    const btnDetails: HTMLElement = document.createElement('button');
+    btnDetails.classList.add('product__btn-details');
+    btnDetails.textContent = 'Details';
     if (router) {
-      btnDetails.addEventListener("click", (e) => {
+      btnDetails.addEventListener('click', (e) => {
         e.preventDefault();
         router.navigate(`products/${id}`);
       });
@@ -122,10 +125,10 @@ class ProductsList {
   }
 
   private createBtnCart(): HTMLElement {
-    const btnCart: HTMLElement = document.createElement("button");
-    btnCart.classList.add("product__btn-cart");
-    const icon: HTMLElement = document.createElement("i");
-    icon.classList.add("icon-shopping-cart");
+    const btnCart: HTMLElement = document.createElement('button');
+    btnCart.classList.add('product__btn-cart');
+    const icon: HTMLElement = document.createElement('i');
+    icon.classList.add('icon-shopping-cart');
     btnCart.append(icon);
     return btnCart;
   }
--- src/utils/products.ts
+++ src/utils/products.ts
@@ -1,49 +1,37 @@
-import { IProduct, IProductsAmount } from "../types/index";
-import productsData from "./products-data";
+import { IProduct, IProductsAmount } from '../types/index';
+import productsData from './products-data';
 
 class Products {
   public getCategories(): string[] {
-    return productsData.products.reduce(
-      (accumulator: string[], currentValue: IProduct) => {
-        if (!accumulator.includes(currentValue.category)) {
-          accumulator.push(currentValue.category);
-        }
-        return accumulator;
-      },
-      []
-    );
+    return productsData.products.reduce((accumulator: string[], currentValue: IProduct) => {
+      if (!accumulator.includes(currentValue.category)) {
+        accumulator.push(currentValue.category);
+      }
+      return accumulator;
+    }, []);
   }
 
   public getBrands(): string[] {
-    return productsData.products.reduce(
-      (accumulator: string[], currentValue: IProduct) => {
-        if (!accumulator.includes(currentValue.brand)) {
-          accumulator.push(currentValue.brand);
-        }
-        return accumulator;
-      },
-      []
-    );
+    return productsData.products.reduce((accumulator: string[], currentValue: IProduct) => {
+      if (!accumulator.includes(currentValue.brand)) {
+        accumulator.push(currentValue.brand);
+      }
+      return accumulator;
+    }, []);
   }
 
   public getCategoriesObject(): Record<string, IProductsAmount> {
-    const categories = productsData.products.reduce(
-      (
-        accumulator: Record<string, IProductsAmount>,
-        currentValue: IProduct
-      ): Record<string, IProductsAmount> => {
-        if (!accumulator[currentValue.category]) {
-          accumulator[currentValue.category] = {
-            all: 1,
-            filter: 0,
-          };
-        } else {
-          accumulator[currentValue.category].all += 1;
-        }
-        return accumulator;
-      },
-      {}
-    );
+    const categories = productsData.products.reduce((accumulator: Record<string, IProductsAmount>, currentValue: IProduct): Record<string, IProductsAmount> => {
+      if (!accumulator[currentValue.category]) {
+        accumulator[currentValue.category] = {
+          all: 1,
+          filter: 0,
+        };
+      } else {
+        accumulator[currentValue.category].all += 1;
+      }
+      return accumulator;
+    }, {});
 
     const productsFilters = this.getProductsFilters();
     productsFilters.forEach((element) => {
@@ -54,23 +42,17 @@ class Products {
   }
 
   public getBrandsObject(): Record<string, IProductsAmount> {
-    const brands = productsData.products.reduce(
-      (
-        accumulator: Record<string, IProductsAmount>,
-        currentValue: IProduct
-      ): Record<string, IProductsAmount> => {
-        if (!accumulator[currentValue.brand]) {
-          accumulator[currentValue.brand] = {
-            all: 1,
-            filter: 0,
-          };
-        } else {
-          accumulator[currentValue.brand].all += 1;
-        }
-        return accumulator;
-      },
-      {}
-    );
+    const brands = productsData.products.reduce((accumulator: Record<string, IProductsAmount>, currentValue: IProduct): Record<string, IProductsAmount> => {
+      if (!accumulator[currentValue.brand]) {
+        accumulator[currentValue.brand] = {
+          all: 1,
+          filter: 0,
+        };
+      } else {
+        accumulator[currentValue.brand].all += 1;
+      }
+      return accumulator;
+    }, {});
 
     const productsFilters = this.getProductsFilters();
     productsFilters.forEach((element) => {
@@ -145,42 +127,24 @@ class Products {
 
   public getProductsFilters(): IProduct[] {
     const url = new URL(window.location.href);
-    const urlParametersCategory: string[] = url.searchParams.getAll("category");
-    const urlParametersBrand: string[] = url.searchParams.getAll("brand");
-    const urlParametersPrice: string | null = url.searchParams.get("price");
-    const urlParametersStock: string | null = url.searchParams.get("stock");
-    const minMaxPrice: string[] | null = urlParametersPrice
-      ? urlParametersPrice.split(",")
-      : null;
-    const minMaxStock: string[] | null = urlParametersStock
-      ? urlParametersStock.split(",")
-      : null;
+    const urlParametersCategory: string[] = url.searchParams.getAll('category');
+    const urlParametersBrand: string[] = url.searchParams.getAll('brand');
+    const urlParameterPrice: string | null = url.searchParams.get('price');
+    const urlParameterStock: string | null = url.searchParams.get('stock');
+    const minMaxPrice: string[] | null = urlParameterPrice ? urlParameterPrice.split(',') : null;
+    const minMaxStock: string[] | null = urlParameterStock ? urlParameterStock.split(',') : null;
 
     const products: IProduct[] = productsData.products.filter((element) => {
-      if (
-        urlParametersCategory.length &&
-        !urlParametersCategory.includes(element.category)
-      ) {
+      if (urlParametersCategory.length && !urlParametersCategory.includes(element.category)) {
         return false;
       }
-      if (
-        urlParametersBrand.length &&
-        !urlParametersBrand.includes(element.brand)
-      ) {
+      if (urlParametersBrand.length && !urlParametersBrand.includes(element.brand)) {
         return false;
       }
-      if (
-        minMaxPrice &&
-        (element.price < Number(minMaxPrice[0]) ||
-          element.price > Number(minMaxPrice[1]))
-      ) {
+      if (minMaxPrice && (element.price < Number(minMaxPrice[0]) || element.price > Number(minMaxPrice[1]))) {
         return false;
       }
-      if (
-        minMaxStock &&
-        (element.stock < Number(minMaxStock[0]) ||
-          element.stock > Number(minMaxStock[1]))
-      ) {
+      if (minMaxStock && (element.stock < Number(minMaxStock[0]) || element.stock > Number(minMaxStock[1]))) {
         return false;
       }
 
@@ -190,22 +154,46 @@ class Products {
     return products;
   }
 
+  public getProductsFiltersSearch(): IProduct[] {
+    const url = new URL(window.location.href);
+    const urlParameterSearch: string | null = url.searchParams.get('search');
+    if (urlParameterSearch) {
+      urlParameterSearch.toLowerCase();
+    }
+
+    const productsFilters = this.getProductsFilters();
+
+    const products: IProduct[] = productsFilters.filter((element) => {
+      if (
+        (urlParameterSearch && element.brand.toLowerCase().indexOf(urlParameterSearch) !== -1) ||
+        (urlParameterSearch && element.title.toLowerCase().indexOf(urlParameterSearch) !== -1) ||
+        (urlParameterSearch && element.description.toLowerCase().indexOf(urlParameterSearch) !== -1) ||
+        (urlParameterSearch && element.price.toString().indexOf(urlParameterSearch) !== -1) ||
+        (urlParameterSearch && element.discount.toString().indexOf(urlParameterSearch) !== -1) ||
+        (urlParameterSearch && element.rating.toString().indexOf(urlParameterSearch) !== -1) ||
+        (urlParameterSearch && element.stock.toString().indexOf(urlParameterSearch) !== -1) ||
+        (urlParameterSearch && element.brand.toLowerCase().indexOf(urlParameterSearch) !== -1) ||
+        (urlParameterSearch && element.category.toLowerCase().indexOf(urlParameterSearch) !== -1)
+      ) {
+        return true;
+      }
+
+      return false;
+    });
+
+    return products;
+  }
+
   private getProductFilterCategoryBrands(): IProduct[] {
     const url = new URL(window.location.href);
-    const urlParametersCategory: string[] = url.searchParams.getAll("category");
-    const urlParametersBrand: string[] = url.searchParams.getAll("brand");
+    const urlParametersCategory: string[] = url.searchParams.getAll('category');
+    const urlParametersBrand: string[] = url.searchParams.getAll('brand');
 
     const products: IProduct[] = productsData.products.filter((element) => {
-      if (
-        urlParametersCategory.length &&
-        !urlParametersCategory.includes(element.category)
-      ) {
+      if (urlParametersCategory.length && !urlParametersCategory.includes(element.category)) {
         return false;
       }
-      if (
-        urlParametersBrand.length &&
-        !urlParametersBrand.includes(element.brand)
-      ) {
+      if (urlParametersBrand.length && !urlParametersBrand.includes(element.brand)) {
         return false;
       }
 
